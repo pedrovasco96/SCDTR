113d4c84e219bd30a4831fe6c76869fa998f60f4
xadreyux <joao.firmino@tecnico.ulisboa.pt>
2017-12-16 21:31:49 +0000

    cant send float

--- CALIBRA__O/CALIBRA__O.ino
+++ CALIBRA__O/CALIBRA__O.ino
@@ -10,21 +10,20 @@ int c = 1;
 int d = 0;
 int i = 0;
 int j = 0;
-char red;
-int led_active=0;
-int n_done=0;
+int led_active = 0;
+int n_done = 0;
 
 float R = 160;
 float start_time = 0;
 float curr_time = 0;
 
-float al[N] = {-0.7, -1.1};
+float al[N] = { -0.7, -1.1};
 float bl[N] = {4.802, 6.1};
 
-float R_p = R*100.0/255.0;
+float R_p = R * 100.0 / 255.0;
 float o = 0;
 float buff = 0;
-float K[N]={0};
+float K[N] = {0};
 
 void setup() {
   Serial.begin(115200);
@@ -33,83 +32,82 @@ void setup() {
 
   Wire.begin(addr);
   TWAR = (addr << 1) | 1;
-  
+
   Wire.onReceive(receiveEvent);
 
-  
+
   Serial.print("address: ");
-  Serial.print(addr);
-  Serial.println();
+  Serial.println(addr);
 }
 
 void loop() {
 
-    for(i=0;i<N;i++){
-      // le valor LDR
-      if(addr==i){
-        n_done = 0;
-        analogWrite(9,R);
-        Serial.println("led ligated");
-        delay(1000);
-        buff = LUX_value();
-        K[i]=R_p/buff;
-        
-        Wire.beginTransmission(0);
-        Wire.write('a');  
-        Wire.endTransmission();    
-        Serial.println("Permission granted"); 
-      
-        start_time=millis();
-        while(n_done<N-1){
-          delay(50);
-          curr_time=millis();
-          if(curr_time-start_time >= 5000){
-            i--;
-            break;
-          }
-            
-        }
-        
-        analogWrite(9,0);
-           
-        }
-      else{
-        while(led_active==0){
-          delay(50);
-        }
-        Serial.println("got permission");
-        delay(1000);
-        buff = LUX_value();
-        K[i]=R/buff;
-        Wire.beginTransmission(i);
-        Wire.write('b');  
-        Wire.endTransmission();
-        led_active==0;
-      }
-    }
-    
-    for(i=0;i<N;i++){
-      Serial.println(K[i]);
-    }
+  for (i = 0; i < N; i++) {
+    // le valor LDR
+    if (addr == i) {
+      n_done = 0;
+      analogWrite(9, R);
+      Serial.println("led ligated");
+      delay(1000);
+      buff = LUX_value();
+      K[i] = R_p / buff;
 
-    delay(1000);
-    o = LUX_value();
-    
-    Serial.println(o);
-    while(1);
+      Wire.beginTransmission(0);
+      Wire.write('a');
+      Wire.endTransmission();
+      Serial.println("Permission granted");
+
+      start_time = millis();
+      while (n_done < N - 1) {
+        delay(50);
+        curr_time = millis();
+        if (curr_time - start_time >= 5000) {
+          i--;
+          break;
+        }
+
+      }
+
+      analogWrite(9, 0);
+
+    }
+    else {
+      while (led_active == 0) {
+        delay(50);
+      }
+      Serial.println("got permission");
+      delay(1000);
+      buff = LUX_value();
+      K[i] = R / buff;
+      Wire.beginTransmission(i);
+      Wire.write('b');
+      Wire.endTransmission();
+      led_active == 0;
+    }
+  }
+
+  for (i = 0; i < N; i++) {
+    Serial.println(K[i]);
+  }
+
+  delay(1000);
+  o = LUX_value();
+
+  Serial.println(o);
+  while (1);
 }
 
 
-void receiveEvent(int howMany){
-  while(Wire.available() > 0){
-    red = Wire.read();
-    if(red=='b'){
-      n_done++;  
+void receiveEvent(int howMany) {
+  while (Wire.available() > 0) {
+    char red = Wire.read();
+    if (red == 'b') {
+      n_done++;
       Serial.print("one finished ");
       Serial.println(n_done);
     }
-    else if(red=='a'){
-      led_active=1;
+    else if (red == 'a') {
+      led_active = 1;
       Serial.println("other led is active");
     }
   }
@@ -123,13 +121,13 @@ float LUX_value()
   float sensv = analogRead(A0);
 
   // define voltage in LDR
-  sensv=5*(sensv/1023);
+  sensv = 5 * (sensv / 1023);
 
   // define resistance in LDR
-  float sensr=10000*(5/sensv-1);
+  float sensr = 10000 * (5 / sensv - 1);
 
   // converting to Lux
-  return pow(10,-bl[addr]/al[addr])*(pow(sensr,1/al[addr]));
+  return pow(10, -bl[addr] / al[addr]) * (pow(sensr, 1 / al[addr]));
 }
 
 
--- consensusard/consensusard.ino
+++ consensusard/consensusard.ino
@@ -6,23 +6,24 @@
 #define N 2
 
 int addr;
-char red;
 int n_done = 0;
-int led_active=0;
+int led_active = 0;
 int node;
 int light;
-float dn[N]={0}; 
-float cn; 
-float qn; 
-float Kn[N]={2.2, 24.0};; 
-float on; 
+float dn[N] = {0};
+float cn;
+float qn;
+float Kn[N];
+float on;
 float Ln;
+int n_drec = 0;
+float d_rec = 0;
 
 float compute_func (float qn, int cn, float rho, float d_av[], float d[], int node, float y[] )
 {
-  float min_unconstrained = 0.5*qn*pow((d[node]),2) + cn*d[node];
+  float min_unconstrained = 0.5 * qn * pow((d[node]), 2) + cn * d[node];
   for (int i = 1; i <= N; i++)
-    min_unconstrained += y[i]*(d[i]-d_av[i])+(rho/2)*pow((d[i]-d_av[i]),2);
+    min_unconstrained += y[i] * (d[i] - d_av[i]) + (rho / 2) * pow((d[i] - d_av[i]), 2);
   return min_unconstrained;
 }
 
@@ -30,13 +31,13 @@ float consensus_function(float dn[], int node, float cn, float qn, float Kn[], f
 {
   float rho = 0.01;
   int iter = 1; int i;
-  float d_av[N]={0}; float d_best[N]; float y[N] = {0};
+  float d_av[N] = {0}; float d_best[N]; float y[N] = {0};
   int min_best_1[50] = {0};
   int sol_unconstrained; int sol_boundary_linear;
   int sol_boundary_0; int sol_boundary_100;
   int sol_linear_0; int sol_linear_100;
   float z[N];
-  float u1=on-Ln; int u2=0; int u3=100;
+  float u1 = on - Ln; int u2 = 0; int u3 = 100;
   float p[N]; float n;
   float w1; float w2; float w3; float dot;
   float dunc[N]; float dlinb[N]; float db0[N]; float db100[N];
@@ -51,178 +52,188 @@ float consensus_function(float dn[], int node, float cn, float qn, float Kn[], f
 
   for (iter = 0; iter < 50; iter++)
   {
-     min_best_1[iter] = 10000; /*big number*/
-     sol_unconstrained = 1;
-     sol_boundary_linear = 1;
-     sol_boundary_0 = 1;
-     sol_boundary_100 = 1;
-     sol_linear_0 = 1;
-     sol_linear_100 = 1;
-     u1 = on-Ln;
-     u2 = 0;
-     u3 = 100;
-     n=0; dot=0;
-     for (i = 0; i < N; i++)
-     {  
-       d_best[i]=-1;
-       z[i]=-y[i]+rho*d_av[i];
-       p[i]=1/rho;
-       if (i=node)
-       {
-        z[i]-= cn;
-        p[i]=1/(rho+qn); 
-       }
-       n += Kn[i]*Kn[i]*p[i];
-       w1 -=Kn[i]*p[i]*z[i];
-       dunc[i]=p[i]*z[i];
-       dot += dunc[i]*Kn[i];
-     }
-     w2 = -z[node]*p[node];
-     w3 = -w2;
-     /*check feasibility of unconstrained minimum using local constraints*/
-     if (dunc[node] < 0 || dunc[node] > 100 || dot < -u1)
-       sol_unconstrained = 0;
-     /*compute minimum constrained to linear boundary   */
-     for (i = 0; i < N; i++)
-      dlinb[i]=p[i]*(z[i]+Kn[i]/n*(w1-u1));
-      
-     /*check feasibility of minimum constrained to linear boundary*/
-     if (dlinb[node] < 0 || dlinb[node] > 100)
-      sol_boundary_linear = 0;
-     /*compute minimum constrained to 0 boundary*/
-     dot=0;
-     for (i = 0; i < N; i++)
-     {
-      if(i != node)
-        db0[i]=p[i]*z[i];
-      else
-        db0[i]=0;
-      dot +=Kn[i]*db0[i];
-     }
-     /*check feasibility of minimum constrained to 0 boundary*/
-     if (db0[node] > 100 || dot < -u1)
-      sol_boundary_0 = 0;
-     /*compute minimum constrained to 100 boundary*/
-     for (i = 0; i < N; i++)
-      db100[i]=db0[i];
-     db100[node]=100;
-     dot+=100;
-     /*check feasibility of minimum constrained to 100 boundary*/
-     if (db0 < 0 || dot < -u1)
-      sol_boundary_100 = 0;
-     /* compute minimum constrained to linear and zero boundary */
-     common = (rho+qn)/((rho+qn)*n-Kn[node]*Kn[node]);
-     det1 = common;
-     det2 = -Kn[node]*common;
-     det3 = det2;
-     det4 = n*(rho+qn)*common;
-     x1 = det1*w1 + det2*w2;
-     x2 = det3*w1 + det4*w2;
-     v1 = det1*u1;
-     v2 = det3*u1;
-     for (i = 0; i < N; i++)
-     {
-      dblin0[i]=p[i]*z[i]+p[i]*Kn[i]*(x1-v1);
+    min_best_1[iter] = 10000; /*big number*/
+    sol_unconstrained = 1;
+    sol_boundary_linear = 1;
+    sol_boundary_0 = 1;
+    sol_boundary_100 = 1;
+    sol_linear_0 = 1;
+    sol_linear_100 = 1;
+    u1 = on - Ln;
+    u2 = 0;
+    u3 = 100;
+    n = 0; dot = 0;
+    for (i = 0; i < N; i++)
+    {
+      d_best[i] = -1;
+      z[i] = -y[i] + rho * d_av[i];
+      p[i] = 1 / rho;
       if (i = node)
-        dblin0[i] += p[i]*(x2-v2);  
-     }
-     /*check feasibility*/
-     if (dblin0[node] > 100) 
-      sol_linear_0 = 0;
-     /* compute minimum constrained to linear and 100 boundary*/
-     common = (rho+qn)/((rho+qn)*n-Kn[node]*Kn[node]);
-     det1 = common;
-     det2 = Kn[node]*common;
-     det3 = det2;
-     det4 = n*(rho+qn)*common;
-     x1 = det1*w1 + det2*w3;
-     x2 = det3*w1 + det4*w3;
-     v1 = det1*u1 + det2*u3; 
-     v2 = det3*u1 + det4*u3;
-     for (i = 0; i < N; i++)
-     {
-      dblin100[i]=p[i]*z[i]+p[i]*Kn[i]*(x1-v1);
-      if (i = node)
-        dblin100[i] -= p[i]*(x2-v2);  
-     }
-     /*check feasibility*/
-     if (dblin100[node] < 0)
-      sol_linear_100 = 0;
-     /*compute function value and if best store new optimum*/
-     if (sol_unconstrained)
-     {
-         min_unconstrained = compute_func (qn, cn, rho, d_av, dunc, node, y);
-         if (min_unconstrained < min_best_1[iter])
-         {
-             for (i = 0; i < N; i++)
-              d_best[i]=dunc[i];
-             min_best_1[iter] = min_unconstrained;
-         }
-     }
-     /*compute function value and if best store new optimum*/
-     if (sol_boundary_linear)
-     {
-        min_boundary_linear = compute_func (qn, cn, rho, d_av, dlinb, node, y);
-        if (min_boundary_linear < min_best_1[iter])
-        {
-             for (i = 0; i < N; i++)
-              d_best[i]=dlinb[i];
-             min_best_1[iter] = min_boundary_linear;
-        }
-     }
-     /* compute function value and if best store new optimum*/
-     if (sol_boundary_0)
-     { 
-          min_boundary_0 = compute_func (qn, cn, rho, d_av, db0, node, y);
-         if (min_boundary_0 < min_best_1[iter])
-         {
-             for (i = 0; i < N; i++)
-              d_best[i]=db0[i];
-             min_best_1[iter] = min_boundary_0;
-         }
-     }
-     /* compute function value and if best store new optimum*/
-     if (sol_boundary_100)
-     { 
-         min_boundary_100 = compute_func (qn, cn, rho, d_av, db100, node, y);
-         if (min_boundary_100 < min_best_1[iter])
-         {
-             for (i = 0; i < N; i++)
-              d_best[i]=db100[i];
-             min_best_1[iter] = min_boundary_100;
-         }
-     }
-     /* compute function value and if best store new optimum*/
-     if (sol_linear_0) 
-     { 
-         min_linear_0 = compute_func (qn, cn, rho, d_av, dblin0, node, y);
-         if (min_linear_0 < min_best_1[iter])
-         {
-             for (i = 0; i < N; i++)
-              d_best[i]=dblin0[i];
-             min_best_1[iter] = min_linear_0;
-         }
-     }
-     /* compute function value and if best store new optimum*/
-     if (sol_linear_100)
-     { 
-          min_linear_100 = compute_func (qn, cn, rho, d_av, dblin100, node, y);
-         if (min_linear_100 < min_best_1[iter])
-         {
-             for (i = 0; i <N; i++)
-              d_best[i]=dblin100[i];
-             min_best_1[iter] = min_linear_100;
-         }
-     }
+      {
+        z[i] -= cn;
+        p[i] = 1 / (rho + qn);
+      }
+      n += Kn[i] * Kn[i] * p[i];
+      w1 -= Kn[i] * p[i] * z[i];
+      dunc[i] = p[i] * z[i];
+      dot += dunc[i] * Kn[i];
+    }
+    w2 = -z[node] * p[node];
+    w3 = -w2;
+    /*check feasibility of unconstrained minimum using local constraints*/
+    if (dunc[node] < 0 || dunc[node] > 100 || dot < -u1)
+      sol_unconstrained = 0;
+    /*compute minimum constrained to linear boundary   */
+    for (i = 0; i < N; i++)
+      dlinb[i] = p[i] * (z[i] + Kn[i] / n * (w1 - u1));
 
-     
-     //compute average with available knowledge
-    // d1_av = (d1+d2_copy)/2;
-     //update local lagrangian
-   //  y1 = y1 + rho*(d1-d1_av);
-     //send node 1 solution to neighboors
-    // d1_copy = d1;
-   
+    /*check feasibility of minimum constrained to linear boundary*/
+    if (dlinb[node] < 0 || dlinb[node] > 100)
+      sol_boundary_linear = 0;
+    /*compute minimum constrained to 0 boundary*/
+    dot = 0;
+    for (i = 0; i < N; i++)
+    {
+      if (i != node)
+        db0[i] = p[i] * z[i];
+      else
+        db0[i] = 0;
+      dot += Kn[i] * db0[i];
+    }
+    /*check feasibility of minimum constrained to 0 boundary*/
+    if (db0[node] > 100 || dot < -u1)
+      sol_boundary_0 = 0;
+    /*compute minimum constrained to 100 boundary*/
+    for (i = 0; i < N; i++)
+      db100[i] = db0[i];
+    db100[node] = 100;
+    dot += 100;
+    /*check feasibility of minimum constrained to 100 boundary*/
+    if (db0 < 0 || dot < -u1)
+      sol_boundary_100 = 0;
+    /* compute minimum constrained to linear and zero boundary */
+    common = (rho + qn) / ((rho + qn) * n - Kn[node] * Kn[node]);
+    det1 = common;
+    det2 = -Kn[node] * common;
+    det3 = det2;
+    det4 = n * (rho + qn) * common;
+    x1 = det1 * w1 + det2 * w2;
+    x2 = det3 * w1 + det4 * w2;
+    v1 = det1 * u1;
+    v2 = det3 * u1;
+    for (i = 0; i < N; i++)
+    {
+      dblin0[i] = p[i] * z[i] + p[i] * Kn[i] * (x1 - v1);
+      if (i = node)
+        dblin0[i] += p[i] * (x2 - v2);
+    }
+    /*check feasibility*/
+    if (dblin0[node] > 100)
+      sol_linear_0 = 0;
+    /* compute minimum constrained to linear and 100 boundary*/
+    common = (rho + qn) / ((rho + qn) * n - Kn[node] * Kn[node]);
+    det1 = common;
+    det2 = Kn[node] * common;
+    det3 = det2;
+    det4 = n * (rho + qn) * common;
+    x1 = det1 * w1 + det2 * w3;
+    x2 = det3 * w1 + det4 * w3;
+    v1 = det1 * u1 + det2 * u3;
+    v2 = det3 * u1 + det4 * u3;
+    for (i = 0; i < N; i++)
+    {
+      dblin100[i] = p[i] * z[i] + p[i] * Kn[i] * (x1 - v1);
+      if (i = node)
+        dblin100[i] -= p[i] * (x2 - v2);
+    }
+    /*check feasibility*/
+    if (dblin100[node] < 0)
+      sol_linear_100 = 0;
+    /*compute function value and if best store new optimum*/
+    if (sol_unconstrained)
+    {
+      min_unconstrained = compute_func (qn, cn, rho, d_av, dunc, node, y);
+      if (min_unconstrained < min_best_1[iter])
+      {
+        for (i = 0; i < N; i++)
+          d_best[i] = dunc[i];
+        min_best_1[iter] = min_unconstrained;
+      }
+    }
+    /*compute function value and if best store new optimum*/
+    if (sol_boundary_linear)
+    {
+      min_boundary_linear = compute_func (qn, cn, rho, d_av, dlinb, node, y);
+      if (min_boundary_linear < min_best_1[iter])
+      {
+        for (i = 0; i < N; i++)
+          d_best[i] = dlinb[i];
+        min_best_1[iter] = min_boundary_linear;
+      }
+    }
+    /* compute function value and if best store new optimum*/
+    if (sol_boundary_0)
+    {
+      min_boundary_0 = compute_func (qn, cn, rho, d_av, db0, node, y);
+      if (min_boundary_0 < min_best_1[iter])
+      {
+        for (i = 0; i < N; i++)
+          d_best[i] = db0[i];
+        min_best_1[iter] = min_boundary_0;
+      }
+    }
+    /* compute function value and if best store new optimum*/
+    if (sol_boundary_100)
+    {
+      min_boundary_100 = compute_func (qn, cn, rho, d_av, db100, node, y);
+      if (min_boundary_100 < min_best_1[iter])
+      {
+        for (i = 0; i < N; i++)
+          d_best[i] = db100[i];
+        min_best_1[iter] = min_boundary_100;
+      }
+    }
+    /* compute function value and if best store new optimum*/
+    if (sol_linear_0)
+    {
+      min_linear_0 = compute_func (qn, cn, rho, d_av, dblin0, node, y);
+      if (min_linear_0 < min_best_1[iter])
+      {
+        for (i = 0; i < N; i++)
+          d_best[i] = dblin0[i];
+        min_best_1[iter] = min_linear_0;
+      }
+    }
+    /* compute function value and if best store new optimum*/
+    if (sol_linear_100)
+    {
+      min_linear_100 = compute_func (qn, cn, rho, d_av, dblin100, node, y);
+      if (min_linear_100 < min_best_1[iter])
+      {
+        for (i = 0; i < N; i++)
+          d_best[i] = dblin100[i];
+        min_best_1[iter] = min_linear_100;
+      }
+    }
+
+
+
+    for (i = 0; i < N; i++) {
+      Wire.beginTransmission(0);
+      Wire.write('c');
+      Wire.write(d_best[i]);
+      Wire.endTransmission();
+
+      while (n_drec < N - 1) {
+        Serial.println("waiting for other ds");
+        delay(500);
+      }
+      n_drec = 0;
+      d_av[i] = (d_rec + d_best[i]) / N;
+      y[i] = y[i] + rho * (d_best[i] - d_av[i]);
+
+    }
+
   }
   return d_best[node];
 }
@@ -234,54 +245,65 @@ void setup() {
 
   Wire.begin(addr);
   TWAR = (addr << 1) | 1;
-  
+
   Wire.onReceive(receiveEvent);
 
-  
+  cn = 1.0;
+  qn = 0.0;
+  on = 0.0;
+
+  if (addr == 0) {
+    Kn[0] = 2.1;
+    Kn[1] = 24.0;
+    Ln = 20.0;
+  }
+  else if (addr == 1) {
+    Kn[0] = 2.3;
+    Kn[1] = 31.0;
+    Ln = 20.0;
+  }
+
+
   Serial.print("address: ");
-  Serial.print(addr);
-  Serial.println();
+  Serial.println(addr);
 }
 
 void loop()
 {
   // CALIB
-  
-  if(addr==0)
-    node = 0;
-  else
-    node = 1;
 
-  cn=1.0;
-  qn=1.0;
-  on=0.0;
-  Ln=20.0;
-  
+  node = addr;
+
+
   light = consensus_function(dn, node, cn, qn, Kn, on, Ln);
-  
-  dn[node]=light;
-  
-  light = light/100*255;
-  analogWrite(9,light);
 
-  while(1)
-  {
-    
-  }
+  Serial.print("d = ");
+  Serial.println(light);
+
+  light = light / 100.0 * 255.0;
+  analogWrite(9, light);
+
+  while (1);
+
   //PID
 }
 
-void receiveEvent(int howMany){
-  while(Wire.available() > 0){
-    red = Wire.read();
-    if(red=='b'){
-      n_done++;  
+void receiveEvent(int howMany) {
+  while (Wire.available() > 0) {
+    char red = Wire.read();
+    if (red == 'b') {
+      n_done++;
       Serial.print("one finished ");
       Serial.println(n_done);
     }
-    else if(red=='a'){
-      led_active=1;
+    else if (red == 'a') {
+      led_active = 1;
       Serial.println("other led is active");
+    }
+    else if (red == 'c') {
+      float buff = Wire.read();
+      d_rec += buff;
+      n_drec++;
     }
   }
 }
